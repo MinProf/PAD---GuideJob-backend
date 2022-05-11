9e37e67497aa2605f94eb391675a7d6422b32cc3
Alin Vujde <>
2022-05-11 11:53:13 +0300

    Added @Data annotations.

--- src/main/java/com/project/GuideJob/model/User.java
+++ src/main/java/com/project/GuideJob/model/User.java
@@ -1,10 +1,7 @@
 package com.project.GuideJob.model;
 
 import com.project.GuideJob.Role.UserRole;
-import lombok.EqualsAndHashCode;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
-import lombok.Setter;
+import lombok.*;
 import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.core.userdetails.UserDetails;
@@ -20,6 +17,7 @@ import static javax.persistence.GenerationType.AUTO;
 @NoArgsConstructor
 @Entity
 @EqualsAndHashCode
+@Data
 public class User implements UserDetails {
     @Id
     @GeneratedValue(strategy = AUTO)
